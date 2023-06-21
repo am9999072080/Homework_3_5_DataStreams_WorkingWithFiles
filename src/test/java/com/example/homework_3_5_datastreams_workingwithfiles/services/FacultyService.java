0e105143ce62008d5042679166de1b25e52d476f
am999 <am9999072080@gmail.com>
2023-06-21 22:12:22 +0300

    commit_1

--- src/test/java/com/example/homework_3_5_datastreams_workingwithfiles/services/FacultyService.java
+++ src/test/java/com/example/homework_3_5_datastreams_workingwithfiles/services/FacultyService.java
@@ -1,5 +1,19 @@
 package com.example.homework_3_5_datastreams_workingwithfiles.services;
 
-public interface FacultyService {
+import com.example.homework_3_5_datastreams_workingwithfiles.model.Faculty;
 
+import java.util.Collection;
+
+public interface FacultyService {
+    Faculty add(Faculty faculty);
+
+    Faculty remove(Long id);
+
+    Faculty update(Faculty faculty);
+
+    Faculty get(Long id);
+
+    Collection<Faculty> getByNameOrColor(String name, String color);
+
+    Collection<Faculty> getAll();
 }
--- src/test/java/com/example/homework_3_5_datastreams_workingwithfiles/services/StudentService.java
+++ src/test/java/com/example/homework_3_5_datastreams_workingwithfiles/services/StudentService.java
@@ -1,5 +1,19 @@
 package com.example.homework_3_5_datastreams_workingwithfiles.services;
 
-public interface StudentService {
+import com.example.homework_3_5_datastreams_workingwithfiles.model.Student;
 
+import java.util.Collection;
+
+public interface StudentService {
+    Student add(Student student);
+
+    Student remove(Long id);
+
+    Student update(Student student);
+
+    Student get(Long id);
+
+    Collection<Student> getByAge(Integer startAge, Integer endAge);
+
+    Collection<Student> getAll();
 }
